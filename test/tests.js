06b2393cc97ded7f96f978dca7a322d0bc6a1d8d
Hugh Francis <hugh@sanctuary.computer>
2021-09-06 23:11:50 +0200

    Port to ERC721

--- test/tests.js
+++ test/tests.js
@@ -16,7 +16,7 @@ const FIVE_MB = 5 * 1000 * 1000;
 const GANACHE_TESTING_MNEMONIC =
   'style fiber prison rally chaos bundle audit ordinary treat twice nut merry';
 const GANACHE_ADDRESS = `http://localhost:8545`;
-var SPACE_ID;
+var SPACE_ADDRESS;
 
 const asUserAccount = async (index, callback) => {
   try {
@@ -25,16 +25,16 @@ const asUserAccount = async (index, callback) => {
         phrase: GANACHE_TESTING_MNEMONIC
       },
       providerOrUrl: GANACHE_ADDRESS,
-      addressIndex: index
+      addressIndex: index,
+      numberOfAddresses: 3,
     });
 
-    AQVS.setContract(require('@truffle/contract'));
+    AQVS.setTruffleContract(require('@truffle/contract'));
     await AQVS.setEnv('dev');
     await AQVS.setFetch(fetch);
     const web3 = new Web3(provider);
     await AQVS.setWeb3(web3);
-    const publicAddress = (await AQVS.web3.eth.getCoinbase()).toLowerCase();
-
+    const publicAddress = (await web3.eth.getCoinbase()).toLowerCase();
     await callback(publicAddress);
     provider.engine.stop();
   } catch (e) {
@@ -44,9 +44,9 @@ const asUserAccount = async (index, callback) => {
 };
 
 test('it can manage sessions', async t => {
-
   await asUserAccount(Accounts.CREATOR , async () => {
-    const publicAddress = (await AQVS.web3.eth.getCoinbase()).toLowerCase();
+    const web3 = await AQVS.getWeb3();
+    const publicAddress = (await web3.eth.getCoinbase()).toLowerCase();
 
     // Make a session
     const makeSessionResponse = await AQVS.sessions.makeSession(publicAddress);
@@ -76,15 +76,15 @@ test('it can manage sessions', async t => {
 });
 
 test('it can mint spaces', async t => {
-
   await asUserAccount(Accounts.CREATOR, async publicAddress => {
+    const web3 = await AQVS.getWeb3();
     const initialBalance = AQVS.utils.ensureBigNumber(
-      await AQVS.web3.eth.getBalance(publicAddress)
+      await web3.eth.getBalance(publicAddress)
     );
 
-    await AQVS.initContracts();
+    await AQVS.init();
     const aqvsInitialBalance = AQVS.utils.ensureBigNumber(
-      await AQVS.web3.eth.getBalance(AQVS.contracts.aqvs.address)
+      await web3.eth.getBalance(AQVS.controllerContract.address)
     );
 
     // Estimate the weiCostToMintSpace
@@ -96,24 +96,22 @@ test('it can mint spaces', async t => {
     const tx = await AQVS.creators.mintSpace(
       3,
       AQVS.units.mb * 5,
-      AQVS.web3.utils.toWei('1', 'ether'),
+      web3.utils.toWei('1', 'ether'),
       false
     );
     const txGasUsed =
-      AQVS.web3.utils.toBN(tx.receipt.cumulativeGasUsed);
+      web3.utils.toBN(tx.receipt.cumulativeGasUsed);
     const txGasPrice =
-      AQVS.web3.utils.toBN((await AQVS.web3.eth.getTransaction(tx.tx)).gasPrice);
+      web3.utils.toBN((await web3.eth.getTransaction(tx.tx)).gasPrice);
     const txCost =
       txGasUsed.mul(txGasPrice);
 
     const mintEvent = tx.logs.find(l => l.event === 'DidMintSpace');
-    SPACE_ID = mintEvent.args.spaceId.toString();
-    const spaceAddress = mintEvent.args.spaceAddress;
-    t.is(!!SPACE_ID, true);
-    t.is(!!spaceAddress, true);
+    SPACE_ADDRESS = mintEvent.args.spaceAddress;
+    t.is(!!SPACE_ADDRESS, true);
 
     const postBalance = AQVS.utils.ensureBigNumber(
-      await AQVS.web3.eth.getBalance(publicAddress)
+      await web3.eth.getBalance(publicAddress)
     );
     t.is(
       postBalance.toString(),
@@ -121,7 +119,7 @@ test('it can mint spaces', async t => {
     );
 
     const aqvsPostBalance = AQVS.utils.ensureBigNumber(
-      await AQVS.web3.eth.getBalance(AQVS.contracts.aqvs.address)
+      await web3.eth.getBalance(AQVS.controllerContract.address)
     );
     t.is(
       aqvsPostBalance.toString(),
@@ -130,20 +128,18 @@ test('it can mint spaces', async t => {
 
     // The created space is now owned by the creator
     const createdSpaceIds =
-      (await AQVS.spaces.getSpaceIdsCreatedBy(publicAddress)).map(bn => bn.toString());
-    t.is(createdSpaceIds.includes(SPACE_ID), true);
+      (await AQVS.spaces.getSpacesCreatedBy(publicAddress)).map(bn => bn.toString());
+    t.is(createdSpaceIds.includes(SPACE_ADDRESS), true);
 
     // Load the Space Contract
-    const spaceContract = await AQVS.spaces.getSpaceById(SPACE_ID);
-    const spaceContractByAddress = await AQVS.spaces.getSpaceByAddress(spaceAddress);
-    t.is(spaceContract, spaceContractByAddress);
+    const spaceContract = await AQVS.spaces.getSpaceByAddress(SPACE_ADDRESS);
 
     // The AQVS user canModifySpace, but others can not
     t.is((await AQVS.spaces.canModifySpace(spaceContract)), true);
     t.is((await AQVS.spaces.canModifySpace(spaceContract, publicAddress)), true);
     t.is((await AQVS.spaces.canModifySpace(
       spaceContract,
-      AQVS.web3.currentProvider.addresses[1]
+      web3.currentProvider.addresses[1]
     )), false);
 
     // The AQVS user canAccessSpace, but others can not
@@ -151,7 +147,7 @@ test('it can mint spaces', async t => {
     t.is((await AQVS.spaces.canAccessSpace(spaceContract, publicAddress)), true);
     t.is((await AQVS.spaces.canAccessSpace(
       spaceContract,
-      AQVS.web3.currentProvider.addresses[1]
+      web3.currentProvider.addresses[1]
     )), false);
 
     // The AQVS user needs a session to update metadata and files
@@ -161,7 +157,7 @@ test('it can mint spaces', async t => {
 	  t.is(!!currentSession.publicAddress, true);
 
     // The AQVS creator can setSpaceMetadata (which also serves as the ERC1155 token metadata)
-    const response = await AQVS.creators.setSpaceMetadata(SPACE_ID, {
+    const response = await AQVS.creators.setSpaceMetadata(SPACE_ADDRESS, {
       name: 'Rainbow Chan',
       description: 'Spacings',
       image: 'https://f4.bcbits.com/img/a0195171096_16.jpg'
@@ -169,7 +165,7 @@ test('it can mint spaces', async t => {
 	  t.is((await response.json()).status, 'ok');
 
     // The anyone can getSpaceMetadata (which also serves as the ERC1155 token metadata)
-    const getSpaceMetadataResponse = await AQVS.spaces.getSpaceMetadata(SPACE_ID);
+    const getSpaceMetadataResponse = await AQVS.spaces.getSpaceMetadata(SPACE_ADDRESS);
     const metadata = await getSpaceMetadataResponse.json();
 	  t.is(metadata.name, 'Rainbow Chan');
 	  t.is(metadata.description, 'Spacings');
@@ -177,17 +173,17 @@ test('it can mint spaces', async t => {
 
     // The AQVS Creator can deleta all files in the Space
     const deleteAllFilesInSpaceResponse =
-      await AQVS.creators.deleteAllFilesInSpace(SPACE_ID, cookie);
+      await AQVS.creators.deleteAllFilesInSpace(SPACE_ADDRESS, cookie);
 	  t.is((await deleteAllFilesInSpaceResponse.json()).status, 'ok');
 
-    const getSpaceContentsResponse = await AQVS.spaces.getSpaceContents(SPACE_ID, cookie);
+    const getSpaceContentsResponse = await AQVS.spaces.getSpaceContents(SPACE_ADDRESS, cookie);
     const contents = await getSpaceContentsResponse.json();
 	  t.is(contents.length, 0);
 
     // Test an Upload
     const formData = new FormData();
     formData.append('/pixel.png', fs.createReadStream('./test/pixel.png'));
-    const uploadFileResponse = await AQVS.creators.uploadFilesToSpace(SPACE_ID, formData, cookie);
+    const uploadFileResponse = await AQVS.creators.uploadFilesToSpace(SPACE_ADDRESS, formData, cookie);
 	  t.is(
       (await uploadFileResponse.json()).success[0],
       '/pixel.png'
@@ -200,7 +196,8 @@ test('it can mint spaces', async t => {
 test('it can access spaces', async t => {
 
   await asUserAccount(Accounts.BUYER, async (publicAddress) => {
-    const spaceContract = await AQVS.spaces.getSpaceById(SPACE_ID);
+    const web3 = await AQVS.getWeb3();
+    const spaceContract = await AQVS.spaces.getSpaceByAddress(SPACE_ADDRESS);
     t.is((await AQVS.spaces.canAccessSpace(spaceContract)), false);
 
     const accessSpaceTx = await AQVS.spaces.accessSpace(spaceContract);
@@ -216,12 +213,12 @@ test('it can access spaces', async t => {
     let cookie = makeSessionResponse.headers.get('set-cookie');
 	  t.is(!!currentSession.publicAddress, true);
 
-    const getSpaceContentsResponse = await AQVS.spaces.getSpaceContents(SPACE_ID, cookie);
+    const getSpaceContentsResponse = await AQVS.spaces.getSpaceContents(SPACE_ADDRESS, cookie);
 	  t.is((await getSpaceContentsResponse.json()).error.message, 'unauthorized');
 
     // Downloads don't work
-    const proxy = AQVS.ENVIRONMENTS[AQVS.env].proxy;
-    const fetchFileResponse = await fetch(`${proxy}/spaces/${SPACE_ID}/pixel.png`, {
+    const gateway = AQVS.ENVIRONMENTS[AQVS.env].gateway;
+    const fetchFileResponse = await fetch(`${gateway}/spaces/${SPACE_ADDRESS}/pixel.png`, {
       method: 'GET',
       credentials: 'include',
       headers: { cookie }
@@ -230,37 +227,37 @@ test('it can access spaces', async t => {
   });
 
   await asUserAccount(Accounts.CREATOR, async () => {
-    const publicAddress = (await AQVS.web3.eth.getCoinbase()).toLowerCase();
-    const spaceContract = await AQVS.spaces.getSpaceById(SPACE_ID);
+    const web3 = await AQVS.getWeb3();
+    const publicAddress = (await web3.eth.getCoinbase()).toLowerCase();
+    const spaceContract = await AQVS.spaces.getSpaceByAddress(SPACE_ADDRESS);
     const tx = await spaceContract.setPurchasable(true, { from: publicAddress });
     // TODO: Add event to search for
   });
 
   await asUserAccount(Accounts.BUYER, async publicAddress => {
-    await AQVS.initContracts();
+    const web3 = await AQVS.getWeb3();
+    await AQVS.init();
     const aqvsInitialBalance = AQVS.utils.ensureBigNumber(
-      await AQVS.web3.eth.getBalance(AQVS.contracts.aqvs.address)
+      await web3.eth.getBalance(AQVS.controllerContract.address)
     );
 
-    const spaceContract = await AQVS.spaces.getSpaceById(SPACE_ID);
+    const spaceContract = await AQVS.spaces.getSpaceByAddress(SPACE_ADDRESS);
     const tx = await AQVS.spaces.accessSpace(spaceContract);
     const accessEvent = tx.logs.find(l => l.event === 'DidAccessSpace');
-    const spaceId = accessEvent.args.spaceId.toString();
     const spaceAddress = accessEvent.args.spaceAddress;
-    t.is(SPACE_ID, spaceId);
     t.is(spaceAddress, spaceContract.address);
     t.is((await AQVS.spaces.canAccessSpace(spaceContract)), true);
 
     // The created space is now owned by the buyer
     const ownedSpaceIds =
-      (await AQVS.spaces.getSpaceIdsOwnedBy(publicAddress)).map(bn => bn.toString());
-    t.is(ownedSpaceIds.includes(SPACE_ID), true);
+      (await AQVS.spaces.getSpacesOwnedBy(publicAddress)).map(bn => bn.toString());
+    t.is(ownedSpaceIds.includes(SPACE_ADDRESS), true);
 
     const remainingSupply = await AQVS.spaces.remainingSupply(spaceContract);
     t.is(remainingSupply.toNumber(), 2);
 
     const aqvsPostBalance = AQVS.utils.ensureBigNumber(
-      await AQVS.web3.eth.getBalance(AQVS.contracts.aqvs.address)
+      await web3.eth.getBalance(AQVS.controllerContract.address)
     );
 
     t.is(
@@ -270,7 +267,7 @@ test('it can access spaces', async t => {
       (await AQVS.creators.estimateSpaceAccessFeesInWei(
         3,
         AQVS.units.mb * 5,
-        AQVS.web3.utils.toWei('1', 'ether'),
+        web3.utils.toWei('1', 'ether'),
       )).toString()
     );
 
@@ -280,7 +277,7 @@ test('it can access spaces', async t => {
         await AQVS.creators.estimateSpaceAccessFeesInWei(
           3,
           AQVS.units.mb * 5,
-          AQVS.web3.utils.toWei('1', 'ether'),
+          web3.utils.toWei('1', 'ether'),
         )
       ).toString()
     );
@@ -290,12 +287,12 @@ test('it can access spaces', async t => {
     let cookie = makeSessionResponse.headers.get('set-cookie');
 
     // Can access!
-    const getSpaceContentsResponse = await AQVS.spaces.getSpaceContents(SPACE_ID, cookie);
+    const getSpaceContentsResponse = await AQVS.spaces.getSpaceContents(SPACE_ADDRESS, cookie);
     t.is((await getSpaceContentsResponse.json())[0].path, '/pixel.png');
 
     // Test downloading now works!
-    const proxy = AQVS.ENVIRONMENTS[AQVS.env].proxy;
-    const fetchFileResponse = await fetch(`${proxy}/spaces/${SPACE_ID}/pixel.png`, {
+    const gateway = AQVS.ENVIRONMENTS[AQVS.env].gateway;
+    const fetchFileResponse = await fetch(`${gateway}/spaces/${SPACE_ADDRESS}/pixel.png`, {
       method: 'GET',
       credentials: 'include',
       headers: { cookie }
@@ -304,38 +301,38 @@ test('it can access spaces', async t => {
   });
 
   await asUserAccount(Accounts.CREATOR, async publicAddress => {
-    const spaceContract = await AQVS.spaces.getSpaceById(SPACE_ID);
+    const web3 = await AQVS.getWeb3();
+    const spaceContract = await AQVS.spaces.getSpaceByAddress(SPACE_ADDRESS);
 
     // It can add delete an individual file
     const makeSessionResponse = await AQVS.sessions.makeSession(publicAddress);
     let cookie = makeSessionResponse.headers.get('set-cookie');
     const deleteFileInSpaceResponse =
-      await AQVS.creators.deleteFileInSpace(SPACE_ID, '/pixel.png', cookie);
+      await AQVS.creators.deleteFileInSpace(SPACE_ADDRESS, '/pixel.png', cookie);
 	  t.is((await deleteFileInSpaceResponse.json()).status, 'ok');
 
     // It can add space capacity
     const addSpaceCapacityTx
-      = await AQVS.creators.addSpaceCapacityInBytes(SPACE_ID, AQVS.units.mb * 1);
+      = await AQVS.creators.addSpaceCapacityInBytes(SPACE_ADDRESS, AQVS.units.mb * 1);
     const spaceCapacity = await spaceContract.spaceCapacityInBytes();
     t.is(spaceCapacity.toNumber(), AQVS.units.mb * 6);
 
     // It can gift space access
     t.is((await AQVS.spaces.canAccessSpace(
       spaceContract,
-      AQVS.web3.currentProvider.addresses[2]
+      web3.currentProvider.addresses[2]
     )), false);
+
     const giftSpaceAccessTx = await AQVS.creators.giftSpaceAccess(
-      SPACE_ID,
-      AQVS.web3.currentProvider.addresses[2]
+      SPACE_ADDRESS,
+      web3.currentProvider.addresses[2]
     );
     const event = giftSpaceAccessTx.logs.find(l => l.event === 'DidGiftSpaceAccess');
-    const spaceId = event.args.spaceId.toString();
     const spaceAddress = event.args.spaceAddress;
-    t.is(SPACE_ID, spaceId);
     t.is(spaceAddress, spaceContract.address);
     t.is((await AQVS.spaces.canAccessSpace(
       spaceContract,
-      AQVS.web3.currentProvider.addresses[2]
+      web3.currentProvider.addresses[2]
     )), true);
   });
 
